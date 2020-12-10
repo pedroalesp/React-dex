621cfcd23a3e077242ab6d16570ff7741e86cb40
Pedro Silva <pedroalejsp@gmail.com>
2020-12-10 19:08:11 -0300

    Added details part 1

--- src/components/PokemonCards.jsx
+++ src/components/PokemonCards.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import '../assets/styles/PokemonCards.css';
 
 const PokemonCards = ({ name, image }) => {
+  const [classOn, setClassOn] = useState(false);
+
+  const handleOver = () => setClassOn(true);
+
+  const handleOut = () => setClassOn(false);
+
   return (
-    <div className='main'>
+    <div className='main' onMouseOver={handleOver} onMouseOut={handleOut}>
       <img src={image} alt={name} />
       <div className='main-name'>{name}</div>
+      <label className={classOn ? 'detailsOn' : 'detailsOf'}>
+        View details
+      </label>
     </div>
   );
 };
---
+++ src/components/PokemonCardsDetails.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const PokemonCardsDetails = () => <h1>poke cositas</h1>;
+
+export default PokemonCardsDetails;
